0390e13b59a30284d4d3e72f567777c83197cf24
Marcell Barreto <marcellbsouzas@gmail.com>
2025-10-15 21:27:15 -0300

    first commit

---
+++ src/Application/Main.java
@@ -0,0 +1,30 @@
+package Application;
+import Entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.printf("Employee: %s, $%.2f", employee.name, employee.NetSalary());
+
+        System.out.printf("\nWhich percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.IncreaseSalary(percentage);
+
+        System.out.printf("Updated data: %s, $%.2f", employee.name, employee.NetSalary());
+
+    }
+}
